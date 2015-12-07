60fb10a7283d82ab0817122591ed401dd8c856fc
azu <azuciao@gmail.com>
2015-12-07 20:44:47 +0900

    initial commit

---
+++ spec/randomeSpec.js
@@ -0,0 +1,13 @@
+var count = 0;
+describe("random 1", function () {
+    it("should be 1?", function () {
+        count++;
+        expect(count).toBe(1);
+    });
+});
+describe("random 2", function () {
+    it("should be 2?", function () {
+        count++;
+        expect(count).toBe(2);
+    });
+});
